97d10a456ed6316bbd2741456a607c5c1be330f1
Otavio Rodolfo Piske <angusyoung@gmail.com>
2024-12-06 18:53:23 +0100

    Allow providing a directory as input

--- workers/camel-worker/src/main/java/org/brainstorm/worker/main/WorkerMain.java
+++ workers/camel-worker/src/main/java/org/brainstorm/worker/main/WorkerMain.java
@@ -1,6 +1,7 @@
 package org.brainstorm.worker.main;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
@@ -35,8 +36,16 @@ import picocli.CommandLine;
 public class WorkerMain implements Callable<Integer> {
     private static final Logger LOG = LoggerFactory.getLogger(WorkerMain.class);
 
-    @CommandLine.Option(names = {"-f", "--file"}, description = "The integration file to use", required = true)
-    private String file;
+    static class ExclusiveEntries {
+        @CommandLine.Option(names = {"--file"}, description = "The integration file to use", required = true)
+        private String file;
+
+        @CommandLine.Option(names = {"--directory"}, description = "A directory containing route files to execute", required = true)
+        private String directory;
+    }
+
+    @CommandLine.ArgGroup(exclusive = true, multiplicity = "1")
+    ExclusiveEntries exclusiveEntries;
 
     @CommandLine.Option(names = {"-d", "--dependencies"}, description = "The list of dependencies to include in runtime (comma-separated)")
     private String dependenciesList;
@@ -133,27 +142,26 @@ public class WorkerMain implements Callable<Integer> {
         return false;
     }
 
-    public static boolean waitForFile(File routeFile, boolean waitForever) throws IOException, InterruptedException {
+    public static boolean waitForFile(File input, boolean isDirectory, boolean waitForever) throws IOException, InterruptedException {
         int retries = 30;
         int waitSeconds = 1;
 
         WatchService watchService = FileSystems.getDefault().newWatchService();
-        Path path = routeFile.getParentFile().toPath();
+        Path path = isDirectory ? input.toPath() : input.getParentFile().toPath();
 
-        if (routeFile.exists()) {
-            LOG.info("File {} already available", routeFile);
+        if (input.exists()) {
+            LOG.info("File {} already available", input);
             return true;
         }
 
         // We watch for both the file creation and truncation
         path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY);
 
-
         do {
             if (!waitForever) {
-                LOG.info("Waiting {} seconds for {} to be available", retries * waitSeconds, routeFile);
+                LOG.info("Waiting {} seconds for {} to be available", retries * waitSeconds, input);
             } else {
-                LOG.info("Waiting indefinitely for {} to be available", routeFile);
+                LOG.info("Waiting indefinitely for {} to be available", input);
             }
 
             WatchKey watchKey = watchService.poll(1, TimeUnit.SECONDS);
@@ -168,52 +176,67 @@ public class WorkerMain implements Callable<Integer> {
                   It should return a Path object for ENTRY_CREATE and ENTRY_MODIFY events
                  */
                 Object context = event.context();
-                if (!(context instanceof Path)) {
+                if (!(context instanceof Path contextPath)) {
                     LOG.warn("Received an unexpected event of kind {} for context {}", event.kind(), event.context());
                     continue;
                 }
 
-                Path contextPath = (Path) context;
-
-                if (contextPath.toString().equals(routeFile.getName())) {
-                    LOG.debug("File at the build path {} had a matching event of type: {}", routeFile.getParentFile().getPath(),
+                if (contextPath.toString().equals(input.getName())) {
+                    LOG.debug("File at the build path {} had a matching event of type: {}", input.getParentFile().getPath(),
                             event.kind());
 
                     return false;
                 } else {
-                    LOG.debug("Ignoring a watch event at build path {} of type {} for file: {}", routeFile.getParentFile().getPath(),
+                    LOG.debug("Ignoring a watch event at build path {} of type {} for file: {}", input.getParentFile().getPath(),
                             event.kind(), contextPath.getFileName());
                 }
             }
             watchKey.reset();
-        } while (!isFileAvailable(routeFile, retries--, waitForever));
+        } while (!isFileAvailable(input, retries--, waitForever));
 
-        return routeFile.exists();
+        return input.exists();
     }
 
     @Override
     public Integer call() throws Exception {
-        if (file.contains("file://")) {
-            LOG.error("Invalid file {} (do not prefix with file://)", file);
-            return 1;
-        }
+        if (exclusiveEntries.file != null) {
+            if (exclusiveEntries.file.contains("file://")) {
+                LOG.error("Invalid file {} (do not prefix with file://)", exclusiveEntries.file);
+                return 1;
+            }
 
-        if (!waitForFile(new File(file), waitForever)) {
-            return 2;
+            if (!waitForFile(new File(exclusiveEntries.file), false, waitForever)) {
+                return 2;
+            }
+        } else {
+            if (!waitForFile(new File(exclusiveEntries.directory), false, waitForever)) {
+                return 2;
+            }
         }
 
         CamelContext context = new DefaultCamelContext();
 
+        if (exclusiveEntries.file != null) {
+            loadRoute(context, "file://" + exclusiveEntries.file);
+        } else {
+            File directory = new File(exclusiveEntries.directory);
+            final String[] list = directory.list((dir, name) -> name.endsWith(".yaml"));
+
+            if (list != null) {
+                File routeFile = new File(exclusiveEntries.directory, list[0]);
+                loadRoute(context, "file://" + routeFile.getAbsolutePath());
+            } else {
+                LOG.warn("No YAML file is available in the directory");
+                return 3;
+            }
+        }
+
         CountDownLatch launchLatch = new CountDownLatch(1);
-
-        loadRoute(context, "file://" + file);
-
-        context.getRegistry().bind(PipelineEndRoute.PROCESSOR, new ShutdownProcessor(launchLatch));
-
-        context.addRoutes(new DataAcquiredRoute(bootstrapServer, bootstrapPort, producesTo, Topics.ACQUISITION_EVENT));
-        context.addRoutes(new PipelineEndRoute(Topics.ACQUISITION_EVENT));
-
         try {
+            context.getRegistry().bind(PipelineEndRoute.PROCESSOR, new ShutdownProcessor(launchLatch));
+            context.addRoutes(new DataAcquiredRoute(bootstrapServer, bootstrapPort, producesTo, Topics.ACQUISITION_EVENT));
+            context.addRoutes(new PipelineEndRoute(Topics.ACQUISITION_EVENT));
+
             context.start();
         } finally {
             launchLatch.await();
