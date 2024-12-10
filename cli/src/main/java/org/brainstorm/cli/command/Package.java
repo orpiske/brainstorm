c776673ba9c95143d5660da450feae3ea37a1864
Otavio Rodolfo Piske <angusyoung@gmail.com>
2024-12-10 15:29:42 +0100

    Cleanup deprecated CLI package with upload command

--- cli/src/main/java/org/brainstorm/cli/command/Package.java
+++ cli/src/main/java/org/brainstorm/cli/command/Package.java
@@ -17,65 +17,21 @@
 
 package org.brainstorm.cli.command;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URI;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 
-import io.quarkus.rest.client.reactive.QuarkusRestClientBuilder;
-import org.brainstorm.cli.common.Archive;
-import org.brainstorm.cli.common.BrainstormEntry;
-import org.brainstorm.cli.services.AcquisitionService;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "package",
         description = "Create a new brainstorm package", sortOptions = false, subcommands = {PackageAcquisitionWorker.class, PackageTransformationRunner.class})
 public class Package extends BaseCommand {
-    @CommandLine.Option(names = {"--package"}, description = "", arity = "0..1")
-    private String addPackage;
 
-    @CommandLine.Option(names = {"--ingestion"}, description = "", arity = "0..1")
-    private String ingestion;
-
-    @CommandLine.Option(names = {"--address"}, description = "The service address", arity = "0..1", required = true, defaultValue = "http://localhost:8080")
-    private String address;
-
-    AcquisitionService acquisitionService;
 
     @Override
     public void run() {
-        List<BrainstormEntry> packageFiles = new ArrayList<>();
 
-        packageFiles.add(new BrainstormEntry(BrainstormEntry.EntryType.ACQUISITION, ingestion));
-        packageFiles.add(new BrainstormEntry(BrainstormEntry.EntryType.CODE, addPackage));
-
-
-        Archive archive = new Archive(packageFiles);
-        try {
-            Path path = archive.compress();
-
-            upload(path);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     private void upload(Path path) {
-        try {
-            acquisitionService = QuarkusRestClientBuilder.newBuilder()
-                    .baseUri(URI.create(address))
-                    .build(AcquisitionService.class);
 
-            FileInputStream fis = new FileInputStream(path.toFile());
-
-            acquisitionService.addPackage(fis);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
